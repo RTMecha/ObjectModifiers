5bfd0eb299243ecd72605ceef56862eb53bc0d87
RTMecha <mechaprower2022@gmail.com>
2024-04-07 01:05:16 +1100

    For fixing component modifiers

---
+++ Functions/Components/DestroyModifierResult.cs
@@ -0,0 +1,21 @@
+﻿using RTFunctions.Functions.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ObjectModifiers.Functions.Components
+{
+    public class DestroyModifierResult : MonoBehaviour
+    {
+        void OnDestroy()
+        {
+            if (Modifier != null)
+                Modifier.Result = null;
+        }
+
+        public BeatmapObject.Modifier Modifier { get; set; }
+    }
+}
