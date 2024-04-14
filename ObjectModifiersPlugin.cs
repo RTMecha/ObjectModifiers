2838ba5f6c833a2490617dc595ec46df9cbd01df
RTMecha <mechaprower2022@gmail.com>
2024-04-14 22:22:50 +1000

    Version: 1.6.2

--- ObjectModifiersPlugin.cs
+++ ObjectModifiersPlugin.cs
@@ -29,7 +29,7 @@ using RTFunctions.Functions.Optimization;
 
 namespace ObjectModifiers
 {
-    [BepInPlugin("com.mecha.objectmodifiers", "Object Modifiers", "1.6.1")]
+    [BepInPlugin("com.mecha.objectmodifiers", "Object Modifiers", "1.6.2")]
     [BepInDependency("com.mecha.rtfunctions")]
     [BepInProcess("Project Arrhythmia.exe")]
     public class ObjectModifiersPlugin : BaseUnityPlugin
